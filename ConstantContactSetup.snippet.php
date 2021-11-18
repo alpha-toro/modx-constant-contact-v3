b389b0d3c9bc1cea063c81183caee20f25c137e4
RCD Dev <80006202+rocketcitydigital@users.noreply.github.com>
2021-11-18 10:48:57 -0600

    Create ConstantContactSetup.snippet.php

---
+++ ConstantContactSetup.snippet.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * This function can be used to exchange an authorization code for an access token.
+ * Make this call by passing in the code present when the account owner is redirected back to you.
+ * The response will contain an 'access_token' and 'refresh_token'
+ * https://v3.developer.constantcontact.com/api_guide/server_flow.html
+ */
+
+/***
+ * @param $redirectURI - URL Encoded Redirect URI
+ * @param $clientId - API Key
+ * @param $clientSecret - API Secret
+ * @param $code - Authorization Code
+ * @return string - JSON String of results
+ */
+
+$uri_setting = $modx->getObject('modSystemSetting', 'cc_redirect_uri');
+$redirectURI = $uri_setting->get('value'); 
+
+$auth_setting = $modx->getObject('modSystemSetting', 'cc_auth_code');
+$code = $auth_setting->get('value');   // 60 second life time
+
+$id_setting = $modx->getObject('modSystemSetting', 'cc_client_id');
+$clientId = $id_setting->get('value'); 
+
+$sec_setting = $modx->getObject('modSystemSetting', 'cc_client_secret');
+$clientSecret = $sec_setting->get('value'); 
+ 
+function getAccessToken($redirectURI, $clientId, $clientSecret, $code) {
+    // Use cURL to get access token and refresh token
+    $ch = curl_init();
+
+    // Define base URL
+    $base = 'https://idfed.constantcontact.com/as/token.oauth2';
+
+    // Create full request URL
+    $url = $base . '?code=' . $code . '&redirect_uri=' . $redirectURI . '&grant_type=authorization_code&scope=contact_data';
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    // Set authorization header
+    // Make string of "API_KEY:SECRET"
+    $auth = $clientId . ':' . $clientSecret;
+    // Base64 encode it
+    $credentials = base64_encode($auth);
+    // Create and set the Authorization header to use the encoded credentials
+    $authorization = 'Authorization: Basic ' . $credentials;
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array($authorization));
+
+    // Set method and to expect response
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Make the call
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+}
+
+// Step 1
+return getAccessToken($redirectURI, $clientId, $clientSecret, $code);
+
+//Access tokens automatically expire two hours (7,200 seconds) after their last use. Access tokens have a maximum lifetime of 24 hours (86,400 seconds).
